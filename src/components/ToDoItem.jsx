99bc1780f25028b4bea68838f4cd21ac81eddb57
Angela Chiang <achi2048@students.cs.ubc.ca>
2025-11-08 13:46:17 -0800

    fixed some styling for the editing to-do buttons

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import '../App.css';
 
 export default function ToDoItem({ todo, onToggle, onDelete, onEdit }) {
@@ -22,7 +22,7 @@ export default function ToDoItem({ todo, onToggle, onDelete, onEdit }) {
             value={editText}
             onChange={(e) => setEditText(e.target.value)}
           />
-          <div>
+          <div className='to-do-item-buttons'>
           <button onClick={handleSave}>Save</button>
           <button onClick={() => setIsEditing(false)}>Cancel</button>
           </div>
